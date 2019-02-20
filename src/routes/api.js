233a49783175e82d8dfdf46ea029716dd525e676
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2019-02-20 17:29:57 +0000

    .

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,14 +1,7 @@
 'use strict';
 
 import Express from 'express';
-import * as Database from '~/database';
-
-import AuthorizationAPI from '~/api/authorization';
 
 const router = Express.Router();
-
-AuthorizationAPI.findById({
-  _id: '5c6d82c33d018832b8844bb8'
-});
 
 export default router;
