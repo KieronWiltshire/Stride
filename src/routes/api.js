5d0ebb3b66fc3b189e562a4dca7f7987e0db3347
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2019-02-24 21:21:56 +0000

    .

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,13 +1,7 @@
 'use strict';
 
 import Express from 'express';
-import UserAPI from '~/api/user'
 
 const router = Express.Router();
-
-router.get('/test', async function(req, res) {
-  let b = await UserAPI.index();
-  res.json(b);
-})
 
 export default router;
